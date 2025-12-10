666a33d0b7b12ba6a84d4aa65aa0e4e845250430
jvcbyte <jvc8463@gmail.com>
2025-12-10 10:21:25 +0100

    functions5.rs

--- rustlings/exercises/02_functions/functions5.rs
+++ rustlings/exercises/02_functions/functions5.rs
@@ -4,6 +4,6 @@ fn square(num: i32) -> i32 {
 }
 
 fn main() {
-    let answer = square(3);
-    println!("The square of 3 is {answer}");
+    let answer = square(5);
+    println!("The square of 5 is {answer}");
 }
