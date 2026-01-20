9d752f6408732bf5c617c3d4005281d89611bb35
jvcbyte <jvc8463@gmail.com>
2026-01-20 15:27:49 +0100

    fix: improve logging configuration and error handling
    * Simplify `RUST_LOG` and `RUST_BACKTRACE` settings by using a single environment variable
    * Update `env_logger` to filter out unnecessary logs based on `RUST_LOG` setting
    * Improve error handling by displaying more informative error messages when crates.io index update fails
    
    ---
    Files changed:
    M	actix_web_task_service/src/main.rs
    M	actix_web_task_service/target/flycheck0/stderr

--- actix_web_task_service/src/main.rs
+++ actix_web_task_service/src/main.rs
@@ -6,10 +6,8 @@ use deps::ddb::DDBRepository;
 
 #[actix_web::main]
 async fn main() -> Result<()> {
-    std::env::set_var("RUST_LOG", "debug");
-    std::env::set_var("RUST_BACKTRACE", "1");
-    env_logger::init();
-
+    let env = env_logger::Env::default().filter_or("RUST_LOG", "debug");
+    env_logger::Builder::from_env(env).init();
     let config = aws_config::load_from_env().await;
 
     HttpServer::new(move || {
