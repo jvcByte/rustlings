69684b62f3c88a4cd954d13ee90666d9485fcca9
jvcbyte <jvc8463@gmail.com>
2026-02-08 23:33:11 +0100

    if1

--- rustlingsv2/rustlings/exercises/03_if/if1.rs
+++ rustlingsv2/rustlings/exercises/03_if/if1.rs
@@ -4,10 +4,19 @@ fn bigger(a: i32, b: i32) -> i32 {
     // Do not use:
     // - another function call
     // - additional variables
+    if a >= b { a } else { b }
 }
 
 fn main() {
     // You can optionally experiment here.
+    let val1 = bigger(25, 30);
+    println!("Bigger number is: {val1}");
+
+    let val2 = bigger(45, 25);
+    println!("Bigger number is: {val2}");
+
+    let val3 = bigger(15, 15);
+    println!("Bigger number is: {val3}");
 }
 
 // Don't mind this for now :)
