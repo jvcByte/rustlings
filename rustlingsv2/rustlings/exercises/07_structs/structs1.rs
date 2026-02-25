4ba0048a1c3f9187b0f95ab130d56de66adf5dba
jvcbyte <jvc8463@gmail.com>
2026-02-26 00:58:43 +0100

    Here is a detailed commit message with a subject line and bullet points:
    `fix: Improve flycheck output in actix_web_task_service`
    - Update `target/flycheck0/stderr` to include more detailed error messages
    - Update `target/flycheck0/stdout` to provide improved compilation warnings
    - Modify `rustlingsv2/rustlings/exercises/07_structs/structs1.rs` to fix struct definition errors
    - Update `test_rust_codes` and `test_rust_codes.rs` to improve test coverage and accuracy
    
    ---
    Files changed:
    M	actix_web_task_service/target/flycheck0/stderr
    M	actix_web_task_service/target/flycheck0/stdout
    M	rustlingsv2/rustlings/exercises/07_structs/structs1.rs
    M	test_rust_codes
    M	test_rust_codes.rs

--- rustlingsv2/rustlings/exercises/07_structs/structs1.rs
+++ rustlingsv2/rustlings/exercises/07_structs/structs1.rs
@@ -1,16 +1,58 @@
+struct ColorRegularStruct {
+    // TODO: Add the fields that the test `regular_structs` expects.
+    // What types should the fields have? What are the minimum and maximum values for RGB colors?
+    red: u8,
+    green: u8,
+    blue: u8,
+}
+
+struct ColorTupleStruct(
+    /* TODO: Add the fields that the test `tuple_structs` expects */ u8,
+    u8,
+    u8,
+);
+
+#[derive(Debug)]
+struct UnitStruct;
+
 fn main() {
-    let l1 = vec![2, 2, 2];
-    let l2 = vec![3, 3, 3];
+    // You can optionally experiment here.
+}
 
-    let mut return_num: i64 = 0;
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    for i in 0..l1.len() {
-        return_num += l1[i] as i64 * 10_i64.pow(i as u32)
+    #[test]
+    fn regular_structs() {
+        // TODO: Instantiate a regular struct.
+        let green = ColorRegularStruct {
+            red: 0,
+            green: 255,
+            blue: 0,
+        };
+
+        assert_eq!(green.red, 0);
+        assert_eq!(green.green, 255);
+        assert_eq!(green.blue, 0);
     }
 
-    for i in 0..l2.len() {
-        return_num += l2[i] as i64 * 10_i64.pow(i as u32)
+    #[test]
+    fn tuple_structs() {
+        // TODO: Instantiate a tuple struct.
+        let green = ColorTupleStruct(0, 255, 0);
+
+        assert_eq!(green.0, 0);
+        assert_eq!(green.1, 255);
+        assert_eq!(green.2, 0);
     }
 
-    println!("{}", return_num);
+    #[test]
+    fn unit_structs() {
+        // TODO: Instantiate a unit struct.
+        let unit_struct = UnitStruct;
+        let message = format!("{unit_struct:?}s are fun!");
+
+        assert_eq!(message, "UnitStructs are fun!");
+    }
 }
--- test_rust_codes.rs
+++ test_rust_codes.rs
@@ -1,21 +1,29 @@
-
-
-fn add_two_nums(l1: Vec<i32>, l2: Vec<i32>) ->
-
-
-fn main() {
-    let l1 = vec![2, 4, 3];
-    let l2 = vec![5, 6, 4];
-
-    let mut return_num: i64 = 0;
+fn add_two_nums(l1: Vec<u32>, l2: Vec<u32>) -> Vec<u32> {
+    let mut hold_num: i64 = 0;
 
     for i in 0..l1.len() {
-        return_num += l1[i] as i64 * 10_i64.pow(i as u32)
+        hold_num += l1[i] as i64 * 10_i64.pow(i as u32);
+        println!("L1 Index {} is {}", i, l1[i]);
     }
 
     for i in 0..l2.len() {
-        return_num += l2[i] as i64 * 10_i64.pow(i as u32)
+        hold_num += l2[i] as i64 * 10_i64.pow(i as u32);
+        println!("L2 Index {} is {}", i, l2[i]);
     }
 
-    println!("{}", return_num);
+    let return_vec: Vec<u32> = hold_num
+        .to_string()
+        .chars()
+        .rev()
+        .map(|c| c.to_digit(10).unwrap())
+        .collect();
+
+    return_vec
+}
+
+fn main() {
+    let l1 = vec![9, 9, 9, 9, 9, 9, 9];
+    let l2 = vec![9, 9, 9, 9];
+    let result = add_two_nums(l1, l2);
+    println!(" Main Result: {result:?}");
 }
