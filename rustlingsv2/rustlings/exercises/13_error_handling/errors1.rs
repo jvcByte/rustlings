663cdc5b0c7bf73512550b3946f4f3bc0696cb4a
jvcbyte <jvc8463@gmail.com>
2026-03-20 08:55:54 +0100

    errors1.rs

--- rustlingsv2/rustlings/exercises/13_error_handling/errors1.rs
+++ rustlingsv2/rustlings/exercises/13_error_handling/errors1.rs
@@ -4,17 +4,21 @@
 // construct to `Option` that can be used to express error conditions. Change
 // the function signature and body to return `Result<String, String>` instead
 // of `Option<String>`.
-fn generate_nametag_text(name: String) -> Option<String> {
+fn generate_nametag_text(name: String) -> Result<String, String> {
     if name.is_empty() {
         // Empty names aren't allowed
-        None
+        Err("Empty names aren't allowed".to_string())
     } else {
-        Some(format!("Hi! My name is {name}"))
+        Ok(format!("Hi! My name is {name}"))
     }
 }
 
 fn main() {
     // You can optionally experiment here.
+    let name = "Michael".to_string();
+    let result = generate_nametag_text(name);
+
+    println!("{result:#?}");
 }
 
 #[cfg(test)]
